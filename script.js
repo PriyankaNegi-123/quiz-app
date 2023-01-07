2811edd8ba682aa86a688078019fb486868a6ace
Priyanka Negi <negipriyanka2211@gmail.com>
2023-01-07 23:58:40 +0530

    UIcomplete

---
+++ script.js
@@ -0,0 +1,42 @@
+let quesArr = [
+    {
+        ques: '1. What is the full form of "HTML"?',
+        optiona: 'Hyper Tool Markup Language',
+        optionb: 'Hyper Text Markup Language',
+        optionc: 'Hyper Text Makeup Language',
+        optiond: 'Hyper Text Machine Language',
+        correct: 'Hyper Text Markup Language'
+    },
+    {
+        ques: '2. What is the full form of "CSS"?',
+        optiona: 'Cascading Super Sheet',
+        optionb: 'Catching Style Sheet',
+        optionc: 'Catching Solo Sheet',
+        optiond: 'Cascading Style Sheet',
+        correct: 'Cascading Style Sheet'
+    },
+    {
+        ques: '3. What is the full form of "API"?',
+        optiona: 'Application Programming Interchange',
+        optionb: 'Application Programming Interface',
+        optionc: 'Application Proclaiming Interface',
+        optiond: 'Application Programming Introduction',
+        correct: 'Application Programming Interface'
+    },
+    {
+        ques: '1. Which of the following is not a tag name in html?',
+        optiona: 'Label',
+        optionb: 'Span',
+        optionc: 'Pank',
+        optiond: 'Input',
+        correct: 'Pank'
+    },
+    {
+        ques: '1. What of the following is a self-closing tag in HTML?',
+        optiona: 'Hyper Tool Markup Language',
+        optionb: 'Hyper Text Markup Language',
+        optionc: 'Hyper Text Makeup Language',
+        optiond: 'Hyper Text Machine Language',
+        correct: 'Hyper Text Markup Language'
+    }
+]
