84863c0d2992191ab1a74d386498234c1bfb258f
Priyanka Negi <negipriyanka2211@gmail.com>
2023-01-08 15:37:58 +0530

    complete

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let quesArr = [
         optionb: 'Hyper Text Markup Language',
         optionc: 'Hyper Text Makeup Language',
         optiond: 'Hyper Text Machine Language',
-        correct: 'Hyper Text Markup Language'
+        correct: 'b'
     },
     {
         ques: '2. What is the full form of "CSS"?',
@@ -13,7 +13,7 @@ let quesArr = [
         optionb: 'Catching Style Sheet',
         optionc: 'Catching Solo Sheet',
         optiond: 'Cascading Style Sheet',
-        correct: 'Cascading Style Sheet'
+        correct: 'd'
     },
     {
         ques: '3. What is the full form of "API"?',
@@ -21,22 +21,90 @@ let quesArr = [
         optionb: 'Application Programming Interface',
         optionc: 'Application Proclaiming Interface',
         optiond: 'Application Programming Introduction',
-        correct: 'Application Programming Interface'
+        correct: 'b'
     },
     {
-        ques: '1. Which of the following is not a tag name in html?',
+        ques: '4. Which of the following is not a tag name in html?',
         optiona: 'Label',
         optionb: 'Span',
         optionc: 'Pank',
         optiond: 'Input',
-        correct: 'Pank'
+        correct: 'c'
     },
     {
-        ques: '1. What of the following is a self-closing tag in HTML?',
-        optiona: 'Hyper Tool Markup Language',
-        optionb: 'Hyper Text Markup Language',
-        optionc: 'Hyper Text Makeup Language',
-        optiond: 'Hyper Text Machine Language',
-        correct: 'Hyper Text Markup Language'
+        ques: '5. What of the following is a self-closing tag in HTML?',
+        optiona: 'span',
+        optionb: 'div',
+        optionc: 'h1',
+        optiond: 'img',
+        correct: 'd'
     }
 ]
+
+let quesEl = document.getElementById("ques");
+let inputEl = document.getElementsByClassName("answer");
+let contEl = document.querySelector(".container");
+
+let index = 0;
+let totalQues = quesArr.length;
+let rightAns = 0;
+let wrongAns = 0;
+// to load ques
+const loadQues = ()=>{
+    if(index == totalQues){
+        return quizOver();
+    }
+    reset();
+    quesEl.innerHTML = quesArr[index].ques;
+    inputEl[0].nextElementSibling.innerHTML = quesArr[index].optiona;
+    inputEl[1].nextElementSibling.innerHTML = quesArr[index].optionb;
+    inputEl[2].nextElementSibling.innerHTML = quesArr[index].optionc;
+    inputEl[03].nextElementSibling.innerHTML = quesArr[index].optiond;
+}
+
+// to reset input
+const reset = ()=>{
+    Array.from(inputEl).forEach((ele) => {
+        ele.checked = false;
+    })
+}
+
+// to submit ans
+const submit = ()=>{
+    const ans = getAnswer();
+    console.log(ans, quesArr[index].correct)
+    if(ans == quesArr[index].correct){
+        rightAns++;
+    }else{
+        wrongAns++;
+    }
+    index++;
+    loadQues();
+    return;
+}
+
+// to get answer
+const getAnswer = ()=>{
+    let answer;
+    Array.from(inputEl).forEach((ele) => {
+        if(ele.checked){
+            answer = ele.value;
+        }
+    })
+    return answer;
+}
+
+// when questions are over
+const quizOver = ()=>{
+    contEl.innerHTML = `
+    <div style = "text-align: center">
+    <h1 style = "padding: 4rem 2rem 2rem;">Thank You for playing the quiz!</h1>
+    <h3 style = "padding: 0rem 2rem 4rem;">You answered ${rightAns}/${totalQues} questions correct.</h3>
+    `;
+}
+
+loadQues();
+
+
+
+
